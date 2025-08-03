6b8e4222c014c1afc402fd941668afafd3270f48
Marvan Alawi <alawimarvan@gmail.com>
2025-08-03 14:18:14 +0300

    Functions moved to scanner.py and DHCP_Discover class added.

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-import scapy.all as scapy
 
-class HostScanner:
-    def __init__(self, ip_range, network_interface, timeout):
-        self.ip_range = ip_range
-        self.network_interface = network_interface
-        self.timeout = timeout
-    
-    def arp_ping(self):
+import scanner as scanner
 
-        arp_request = scapy.arping(self.ip_range,
-                                   iface=self.network_interface,
-                                   timeout=self.timeout,)
-        return arp_request
-    
-    def icmp_ping(self):
 
-        icmp_request = scapy.sr(scapy.IP(dst=self.ip_range)/scapy.ICMP(),
-                                 iface=self.network_interface,
-                                 timeout=self.timeout)
-        return icmp_request[0].summary() if icmp_request else "No response"
-    
 if __name__ == "__main__":
-    scanner = HostScanner("192.168.1.0/24", "Ethernet", 2)
-    icmp_result = scanner.icmp_ping()
-    print(f"ICMP Ping Result: {icmp_result}")
+    dhcp_scanner = scanner.DHCPScanner("Ethernet")  # Buraya kendi arayüz adını yaz (örnek: "eth0", "wlan0")
+    dhcp_scanner.send_discover()
+    dhcp_scanner.listen_response()
